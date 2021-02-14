1ff94d1bfb0afdb986828a4a7ce1c7c7fbcfdfc9
thomas royal <thomas.m.royal@gmail.com>
2021-02-13 20:22:22 -0500

    ADSR Envelopes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function startAudio(){
   SynthEngine.setVolume(0.1);
 
   const synth = SynthEngine.createSynth();
+  synth.setADSR(0.1, 0.1, 0.0, 0.0);
   synth.play(60,0.5,1);
   synth.play(64,0.5,3);
 }
--- src/synthEngine/Synth.js
+++ src/synthEngine/Synth.js
@@ -1,5 +1,7 @@
 import SynthUtil from './SynthUtil';
 
+const REL_PADDING = 50;
+
 export default class Synth {
   /**
    * Creates a new Synth
@@ -7,6 +9,22 @@ export default class Synth {
    */
   constructor(engine){
     this.engine = engine;
+    this.setADSR(0.01, 0.1, 0.1, 0.1);
+  }
+
+  /**
+   * set Attack, sustain, and release settings for the envelope
+   * 
+   * @param {float} attack time
+   * @param {float} decay time 0-1
+   * @param {float} sustain level 0-1
+   * @param {float} release time
+   */
+  setADSR(attack, decay, sustain, release){
+    this.attack = attack;
+    this.decay = decay;
+    this.sustain = sustain;
+    this.release = release;
   }
 
   /**
@@ -18,13 +36,47 @@ export default class Synth {
    */
   play(note, volume, dur){
     const osc = this.engine.audioContext.createOscillator();
-    osc.connect(this.engine.gainNode);
+    const env = this.engine.audioContext.createGain();
+
+    osc.connect(env).connect(this.engine.gainNode);
     osc.type = 'sine';
     osc.frequency.value = SynthUtil.midicps(note);
+
+    env.gain.value = 0.0;
+
     osc.start();
+
+    this.setEnvelope(volume, dur, env);
 
     setTimeout(()=>{
       osc.stop();
-    }, dur*1000.0);
+    }, dur*1000.0 + this.release*1000.0 + REL_PADDING);
+  }
+
+  /**
+   * set envelope segements
+   * 
+   * @param {float} volume - max volume
+   * @param {GainNode} dur - sound duration in seconds
+   * @param {GainNode} gain - gain object
+   */
+  setEnvelope(volume, dur, gain){
+    let time = this.engine.audioContext.currentTime;
+
+    // attack
+    time += this.attack;
+    gain.gain.linearRampToValueAtTime(volume, time);
+
+    // decay
+    time += this.decay;
+    gain.gain.linearRampToValueAtTime(this.sustain, time);
+
+    // sustain
+    time += dur;
+    gain.gain.linearRampToValueAtTime(this.sustain, time);
+
+    // release
+    time += this.release;
+    gain.gain.linearRampToValueAtTime(0, time);
   }
 };
