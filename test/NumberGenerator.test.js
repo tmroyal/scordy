16c61da900ca378eaf5fe73b93a7a044e86f16d2
thomas royal <thomas.m.royal@gmail.com>
2021-02-25 16:10:00 -0500

    Randint tests

--- test/NumberGenerator.test.js
+++ test/NumberGenerator.test.js
@@ -2,20 +2,46 @@ import NumberGenerators from '../src/NumberGenerators/NumberGenerators.mjs'
 var expect = require('chai').expect;
 
 describe('Number Generators', ()=>{
+  const min = -3;
+  const max = 4;
+
   describe('randint', ()=>{
-    it('should generate numbers between hi and low', ()=>{
-      const low = -3;
-      const high = 4;
+    it('should generate numbers from min to max', ()=>{
       let matches = true;
       let testValue;
       for (let i = 0; i < 50; i++){
         testValue = NumberGenerators.randint(-3,4);
-        matches = (testValue < high) && (testValue >= low);
+        matches = (testValue <= max) && (testValue >= min);
         if (!matches){
           break;
         }
       }
       expect(matches).to.be.true;
+    });
+
+    // this test may not pass, being stochastic
+    it('should eventually generate min', ()=>{
+      let eq = false;
+      for (var i = 0; i < 50; i++){
+        if (NumberGenerators.randint(min, max) === min){
+          eq = true;
+          break;
+        }
+      }
+      expect(eq).to.be.true;
+    });
+
+    it('should eventually generate max', ()=>{
+      let eq = false;
+
+      for (var i = 0; i < 50; i++){
+        if (NumberGenerators.randint(min, max) === max){
+          eq = true;
+          break;
+        }
+      }
+
+      expect(eq).to.be.true;
     });
 
   })
